78123fdd55a4dd2e0622729744b86af5a761c0c4
rqthomas <rqthomas@vt.edu>
2023-02-03 12:53:09 -0500

    initial creation

---
+++ R/plot_temp.R
@@ -0,0 +1,39 @@
+#' Faceted plot of water temperature
+#'
+#' @param score_df data frame to be ploted
+#' @param depths vector of depths to be facets
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_temp <- function(score_df, depths = 0.5) {
+
+  # Generate labels for plots
+  my_breaks <- lubridate::with_tz(seq(min(score_df$datetime), max(score_df$datetime), by = "1 day"),"America/New_York")
+  my_label <- seq(lubridate::as_datetime(score_df$reference_datetime)[1], max(score_df$datetime), by = "5 days")
+  my_labels <- as.character(my_breaks)
+  my_labels[which(!(my_breaks %in% my_label))] <- " "
+  y_label <- expression(paste('Water temperature (',degree,'C)', sep = ""))
+
+  # Generate the pot
+  score_df |>
+    # Filter the score_df and get in the right format
+    dplyr::filter(depth %in% depths) |>
+    dplyr::mutate(datetime = lubridate::with_tz(lubridate::as_datetime(datetime), "America/New_York"),
+                  reference_datetime = lubridate::with_tz(lubridate::as_datetime(reference_datetime), "America/New_York"),
+                  depth = paste0("Depth: ", depth)) |>
+    dplyr::filter(datetime >= reference_datetime) |>
+
+    ggplot(aes(x = datetime)) +
+    geom_ribbon(aes(ymin = quantile10, ymax = quantile90), fill = "lightblue", color = "lightblue") +
+    geom_line(aes(y = mean)) +
+    scale_x_continuous(breaks = my_breaks, labels = my_labels) +
+    facet_wrap(~depth) +
+    labs(y = y_label) +
+    ylim(c(-5,35)) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.2)) +
+    theme(text = element_text(size = 20))
+}
+
