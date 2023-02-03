00680dbd951ae927b151a0d60f6ec9bec0d24eb1
rqthomas <rqthomas@vt.edu>
2023-02-03 13:02:07 -0500

    pulling out hard coded time zone and temperature axis limits

--- R/plot_temp.R
+++ R/plot_temp.R
@@ -1,16 +1,18 @@
 #' Faceted plot of water temperature
 #'
-#' @param score_df data frame to be ploted
+#' @param score_df data frame to be plotted
 #' @param depths vector of depths to be facets
+#' @param tzone time zone for datetime in plot
+#' @param ylims lower,upper bounds of temperature on plot
 #'
 #' @return
 #' @export
 #'
 #' @examples
-plot_temp <- function(score_df, depths = 0.5) {
+plot_temp <- function(score_df, depths = 0.5, tzone = "America/New_York", ylims = c(-5,35)) {
 
   # Generate labels for plots
-  my_breaks <- lubridate::with_tz(seq(min(score_df$datetime), max(score_df$datetime), by = "1 day"),"America/New_York")
+  my_breaks <- lubridate::with_tz(seq(min(score_df$datetime), max(score_df$datetime), by = "1 day"), tzone)
   my_label <- seq(lubridate::as_datetime(score_df$reference_datetime)[1], max(score_df$datetime), by = "5 days")
   my_labels <- as.character(my_breaks)
   my_labels[which(!(my_breaks %in% my_label))] <- " "
@@ -20,8 +22,8 @@ plot_temp <- function(score_df, depths = 0.5) {
   score_df |>
     # Filter the score_df and get in the right format
     dplyr::filter(depth %in% depths) |>
-    dplyr::mutate(datetime = lubridate::with_tz(lubridate::as_datetime(datetime), "America/New_York"),
-                  reference_datetime = lubridate::with_tz(lubridate::as_datetime(reference_datetime), "America/New_York"),
+    dplyr::mutate(datetime = lubridate::with_tz(lubridate::as_datetime(datetime), tzone),
+                  reference_datetime = lubridate::with_tz(lubridate::as_datetime(reference_datetime), tzone),
                   depth = paste0("Depth: ", depth)) |>
     dplyr::filter(datetime >= reference_datetime) |>
 
@@ -31,7 +33,7 @@ plot_temp <- function(score_df, depths = 0.5) {
     scale_x_continuous(breaks = my_breaks, labels = my_labels) +
     facet_wrap(~depth) +
     labs(y = y_label) +
-    ylim(c(-5,35)) +
+    ylim(ylims) +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.2)) +
     theme(text = element_text(size = 20))
