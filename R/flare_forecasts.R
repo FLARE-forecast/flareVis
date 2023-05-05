101321f8b75e4798d26311263ad64c24160a3c3d
addelany <addelany@gmail.com>
2023-05-05 17:41:36 -0500

    update viz figures

--- R/flare_forecasts.R
+++ R/flare_forecasts.R
@@ -1,18 +1,13 @@
 
 forecast_s3 <- arrow::s3_bucket('forecasts/parquet', endpoint_override = 's3.flare-forecast.org', anonymous = TRUE)
 
-forecast_data <- arrow::open_dataset(forecast_s3) |>
-  dplyr::filter(site_id == 'sunp',
-                #reference_datetime > lubridate::as_datetime('2022-10-01 00:00:00'),
-                reference_datetime == '2023-05-01 00:00:00',
-                variable == 'temperature',
-                depth == 1.0)
-noaa_forecasts <- function(forecast_bucket, site, variable_name, forecast_start_date, plot_depth){
+
+flare_forecasts <- function(forecast_bucket, site, variable_name, forecast_start_date, plot_depth){
 
   forecast_data <- arrow::open_dataset(forecast_bucket) |>
     dplyr::filter(site_id == site,
                   #reference_datetime > lubridate::as_datetime('2022-10-01 00:00:00'),
-                  reference_datetime == lubridate::as_datetime(forecast_start_date),
+                  reference_datetime ==  forecast_start_date,
                   variable == variable_name,
                   depth == plot_depth) |>
     collect()
@@ -30,24 +25,21 @@ noaa_forecasts <- function(forecast_bucket, site, variable_name, forecast_start_
     p <- ggplot(forecast_data, aes(x=datetime,y=prediction, group = parameter)) +
       geom_line() +
       ylab('Temperature (deg F)') +
-      ggtitle('FLARE Forecast Members')
+      xlab('Date')
+      ggtitle('FLARE Forecast Members') +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
 
   } else {
-    noaa_noon <- forecast_data |>
-      mutate(time = format(as.POSIXct(datetime), format = '%H:%M')) |>
-      filter(time == '12:00') |>
-      mutate(datetime = format(as.POSIXct(datetime), format = '%Y-%m-%d'))
-
-    noaa_noon$datetime <- as.Date(noaa_noon$datetime)
 
     p <- ggplot(forecast_data, aes(x=datetime,y=prediction, group = parameter)) +
       geom_line()
   }
 
-  p
+  return(p)
 
-  invisible(noaa_data)
+  #invisible(forecast_data)
 }
 
-sunp_forecast <- noaa_forecasts(forecast_bucket = forecast_s3, site = 'sunp', variable_name = 'temperature',forecast_start_date = '2023-05-01 00:00:00', depth = 1.0)
+flare_forecasts(forecast_bucket = forecast_s3, site = 'sunp', variable_name = 'temperature', forecast_start_date = '2023-05-01 00:00:00', plot_depth = 1.0)
 
--- R/noaa_forecasts.R
+++ R/noaa_forecasts.R
@@ -1,15 +1,15 @@
 #' @param forecast_start_date forecast reference_datetime value (yyy-mm-dd HH:MM:SS)
 
 noaa_s3 <- arrow::s3_bucket("drivers/noaa/gefs-v12-reprocess/stage2/parquet/0", endpoint_override = "s3.flare-forecast.org", anonymous = TRUE)
-
-noaa_data <- arrow::open_dataset(noaa_s3) |>
-  dplyr::filter(site_id == "sunp",
-                #reference_datetime > lubridate::as_datetime('2022-10-01 00:00:00'),
-                reference_datetime == lubridate::as_datetime('2023-04-01 00:00:00'),
-                horizon < 408, # just want first 16 days of horizon
-                variable == 'air_temperature') %>%
-  filter(variable == 'air_temperature') |>
-  collect()
+#
+# noaa_data <- arrow::open_dataset(noaa_s3) |>
+#   dplyr::filter(site_id == "sunp",
+#                 #reference_datetime > lubridate::as_datetime('2022-10-01 00:00:00'),
+#                 reference_datetime == lubridate::as_datetime('2023-04-01 00:00:00'),
+#                 horizon < 408, # just want first 16 days of horizon
+#                 variable == 'air_temperature') %>%
+#   filter(variable == 'air_temperature') |>
+#   collect()
 
 
 noaa_forecasts <- function(noaa_bucket, site, variable_name, forecast_start_date){
@@ -34,7 +34,10 @@ noaa_forecasts <- function(noaa_bucket, site, variable_name, forecast_start_date
     p <- ggplot(noaa_noon, aes(x=datetime,y=prediction, group = parameter)) +
       geom_line() +
       ylab('Temperature (deg F)') +
-      ggtitle('NOAA Ensemble Forecasts')
+      xlab('Date')
+      ggtitle('NOAA Ensemble Forecasts') +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
 
   } else {
     noaa_noon <- noaa_data |>
@@ -48,10 +51,10 @@ noaa_forecasts <- function(noaa_bucket, site, variable_name, forecast_start_date
       geom_line()
   }
 
-  p
+  return(p)
 
-  invisible(noaa_data)
+  #invisible(noaa_data)
 }
 
-sunp_noaa <- noaa_forecasts(noaa_bucket = noaa_s3, site = 'sunp', variable_name = 'air_temperature',forecast_start_date = '2023-05-01 00:00:00')
+sunp_noaa <- noaa_forecasts(noaa_bucket = noaa_s3, site = 'sunp', variable_name = 'air_temperature', forecast_start_date = '2023-05-01 00:00:00')
 
