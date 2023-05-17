f121ae4a16062890f2f580423b2bffdfb5b43298
Heather Wander <hwander@vt.edugit config --global user.name Heather>
2023-05-17 14:40:50 -0400

    specifying depth and model_id in function

--- R/past_obs_future_forecast.R
+++ R/past_obs_future_forecast.R
@@ -8,58 +8,63 @@
 #' @export
 #'
 
-#note - still working on being able to specify differennt depths, groups, and horizons
 past_obs_future_forecast <-
-  function(data, depths = 0.5, tzone = "America/New_York", ylim = NULL ,site_name = "", variable_name= NA, group_id = NA) {
+  function(data, tzone = "America/New_York", depth = NULL,
+           ylim = NULL , site_name = "", variable_name = NA, group_id = NA) {
 
     if (variable_name == 'temperature'){
 
-      time_plot <- ggplot(data = full_data, aes(datetime, observation, color=depth)) +
+      time_plot <- ggplot(data = full_data, aes(datetime, observation, color=as.factor(depth))) +
         geom_point() +
         geom_line() +
+        facet_wrap(~as.factor(get(group_id))) +
         geom_line(aes(datetime,median)) +
         geom_line(aes(datetime,quantile97.5)) +
         geom_line(aes(datetime,quantile02.5)) +
         geom_vline(xintercept = as.Date(forecast_date), linetype = 'dashed') +
-        ylab('Temperature (degC)') +
-        xlab('Date') +
-        annotate("text", x=as.Date(forecast_date) -10, y=27, label= "Past") +
+        ylab(expression("Temperature ("*degree*C*")")) +
+        xlab('Date') +  labs(color='Depth (m)') +
+        annotate("text", x=as.Date(forecast_date) -7, y=27, label= "Past") +
         annotate("text", x=as.Date(forecast_date) +7, y=27, label= "Future")
 
 
       print(time_plot)
+    }
+  }
 
-    }
-    }
+
 
 #set date for forecast
-forecast_date <- '2023-04-01 00:00:00'
+forecast_date <- '2022-04-04 00:00:00'
+use_s3 <- FALSE
 
 ## READ IN DATA
-scores_s3 <- arrow::s3_bucket('scores/parquet', endpoint_override = 's3.flare-forecast.org', anonymous = TRUE)
+  scores_s3 <- arrow::s3_bucket('scores/ler_ms3/parquet',
+                                             endpoint_override = 's3.flare-forecast.org',
+                                             anonymous = TRUE)
 
-scores <- arrow::s3_bucket('scores/parquet/', endpoint_override = 's3.flare-forecast.org') %>%
-     arrow::open_dataset() %>%
-     filter(reference_datetime >= '2023-03-01 00:00:00',
-            reference_datetime <= '2023-04-30 00:00:00',
-            site_id == 'fcre',
-            variable == 'temperature',
-            depth == 1) %>%
-     collect()
+  scores <- arrow::open_dataset(scores_s3) |>
+    filter(reference_datetime >= '2022-03-01 00:00:00',
+           reference_datetime <= '2022-04-30 00:00:00',
+           site_id == 'fcre',
+           variable == 'temperature',
+           depth %in% c(1,7)) |>
+    collect()
 
-past_obs <- scores %>%
-  filter(datetime < forecast_date) %>%
-  drop_na(observation) %>%
-  distinct(reference_datetime, observation, .keep_all=TRUE) %>%
-  select(observation, depth, datetime)
+past_obs <- scores |>
+  filter(reference_datetime <= forecast_date) |>
+  filter(reference_datetime == as.Date(datetime)) |>
+  drop_na(observation) |>
+  distinct(reference_datetime, observation, model_id, .keep_all=TRUE) |>
+  select(observation, depth, reference_datetime, datetime, model_id)
 
 
-future_predictions <- scores %>%
+future_predictions <- scores |>
   filter(reference_datetime == forecast_date,
-         datetime != (as.Date(forecast_date) -1)) %>%
-  select(-observation)
+         datetime >= (as.Date(forecast_date))) |>
+  select(-c(observation,reference_datetime))
 
-full_data <- full_join(future_predictions,past_obs, by = c('depth', 'datetime'))
+full_data <- full_join(future_predictions,past_obs, by = c('depth', 'datetime', 'model_id'))
 
 full_data$reference_datetime <- as.character(full_data$reference_datetime)
 full_data$datetime <- as.character(full_data$datetime)
@@ -69,6 +74,9 @@ full_data$datetime <- as.Date(full_data$datetime)
 
 
 ## CALL FUNCTION AND DISPLAY PLOT
-forecast_fig <- past_obs_future_forecast(scores, tzone = NULL, ylim= NULL, variable_name = "temperature",group_id = model_id)
+forecast_fig <- past_obs_future_forecast(full_data, tzone = "America/New_York",
+                                         variable_name = "temperature", depth = "depth",
+                                         ylim = NULL , group_id = 'model_id')
 
 forecast_fig
+
